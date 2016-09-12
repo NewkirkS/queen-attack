4a332adde7361bbefade0267542d381f1d87255b
Stephen Newkirk <newkirk771@gmail.com>
2016-09-12 13:38:11 -0700

    Add basic skeleton code for Queen class in src doc including constructor and empty function for calulating attack boolean.

---
+++ src/Queen.php
@@ -0,0 +1,18 @@
+<?php
+    class Queen
+    {
+        $x = 0;
+        $y = 0;
+
+        function __construct($x, $y)
+        {
+            $this->x = $x;
+            $this->y = $y;
+        }
+
+        function canAttack($targetX, $targetY)
+        {
+            
+        }
+    }
+ ?>
