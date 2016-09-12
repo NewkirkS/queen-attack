014e4584775e9bb08b3f759af32967bf2b501433
Stephen Newkirk <newkirk771@gmail.com>
2016-09-12 16:15:18 -0700

    Create twig pages and corresponding routes.

--- app/app.php
+++ app/app.php
@@ -1,1 +1,24 @@
+<?php
+    date_default_timezone_set("America/Los_Angeles");
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Queen.php";
 
+    $app = new Silex\Application();
+
+    $app["debug"] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        "twig.path" => __DIR__."/../views"
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app["twig"]->render("form.html.twig");
+    });
+
+    $app->get("/result", function() use ($app) {
+        $newQueen = new Queen($_GET["x_coordinate_queen"], $_GET["y_coordinate_queen"]);
+        return $app["twig"]->render("result.html.twig", array("result" => $newQueen->canAttack($_GET["x_coordinate_target"], $_GET["y_coordinate_target"])));
+    });
+
+    return $app;
+ ?>
